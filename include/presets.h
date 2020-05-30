32e36dd957816443e9b390bac6542f618911ac4f
Emre Can <emredjan@gmail.com>
2020-05-30 02:40:15 +0200

    Add commands and presets

--- include/presets.h
+++ include/presets.h
@@ -6,7 +6,45 @@
 void callPreset(byte bank, byte program);
 
 void preset_1_1();
-
+void preset_1_2();
+void preset_1_3();
+void preset_1_4();
+void preset_2_1();
+void preset_2_2();
+void preset_2_3();
+void preset_2_4();
+void preset_3_1();
+void preset_3_2();
+void preset_3_3();
+void preset_3_4();
+void preset_3_1();
+void preset_3_2();
+void preset_3_3();
+void preset_3_4();
+void preset_4_1();
+void preset_4_2();
+void preset_4_3();
+void preset_4_4();
+void preset_5_1();
+void preset_5_2();
+void preset_5_3();
+void preset_5_4();
+// void preset_6_1();
+// void preset_6_2();
+// void preset_6_3();
+// void preset_6_4();
+// void preset_7_1();
+// void preset_7_2();
+// void preset_7_3();
+// void preset_7_4();
+// void preset_8_1();
+// void preset_8_2();
+// void preset_8_3();
+// void preset_8_4();
+// void preset_9_1();
+// void preset_9_2();
+// void preset_9_3();
+// void preset_9_4();
 
 
 #endif
--- src/commands.cpp
+++ src/commands.cpp
@@ -5,7 +5,6 @@
 #include <midiclock.h>
 #include <globals.h>
 
-
 void callCommand(byte program)
 {
 
@@ -59,25 +58,122 @@ void callCommand(byte program)
 
 void command_1()
 {
+    if (!command_sent[1 - 1])
+    {
+        MIDI.sendProgramChange(2, CH_MODFACTOR); // ModFactor Chorus
+        setOled(F("CHORS"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(125, CH_MODFACTOR); // ModFactor Bypass
+        setOled(F("CHORS"), F("OFF"));
+    }
+    command_sent[1 - 1] = !command_sent[1 - 1];
 }
+
 void command_2()
 {
+    if (!command_sent[2 - 1])
+    {
+        MIDI.sendProgramChange(3, CH_MODFACTOR); // ModFactor Phaser
+        setOled(F("PHASR"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(125, CH_MODFACTOR); // ModFactor Bypass
+        setOled(F("PHASR"), F("OFF"));
+    }
+    command_sent[2 - 1] = !command_sent[2 - 1];
 }
+
 void command_3()
 {
+    if (!command_sent[3 - 1])
+    {
+        MIDI.sendProgramChange(11, CH_MODFACTOR); // ModFactor Rotary
+        setOled(F("ROTRY"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(125, CH_MODFACTOR); // ModFactor Bypass
+        setOled(F("ROTRY"), F("OFF"));
+    }
+    command_sent[3 - 1] = !command_sent[3 - 1];
 }
+
 void command_4()
 {
+    if (!command_sent[4 - 1])
+    {
+        MIDI.sendProgramChange(13, CH_MODFACTOR); // ModFactor Tremolo
+        setOled(F("TREM"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(125, CH_MODFACTOR); // ModFactor Bypass
+        setOled(F("TREM"), F("OFF"));
+    }
+    command_sent[4 - 1] = !command_sent[4 - 1];
 }
+
 void command_5()
 {
+    if (!command_sent[5 - 1])
+    {
+        MIDI.sendProgramChange(5, CH_MODFACTOR); // ModFactor Wah
+        setOled(F("WAH"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(125, CH_MODFACTOR); // ModFactor Bypass
+        setOled(F("WAH"), F("OFF"));
+    }
+    command_sent[5 - 1] = !command_sent[5 - 1];
 }
+
 void command_6()
 {
+    if (!command_sent[6 - 1])
+    {
+        MIDI.sendProgramChange(7, CH_MODFACTOR); // ModFactor Flanger
+        setOled(F("FLNGR"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(125, CH_MODFACTOR); // ModFactor Bypass
+        setOled(F("FLNGR"), F("OFF"));
+    }
+    command_sent[6 - 1] = !command_sent[6 - 1];
 }
+
+
 void command_7()
 {
+    tapTempo();
+
+    byte displayPrint[3];
+
+    if (bpm != prevBpm)
+    {
+        for (int i = 0; i < 3; i++)
+            displayPrint[i] = getNumberToPrint(bpm)[i];
+        sevenSeg.setAll(displayPrint);
+    }
+
+    setOled(F("BPM"), F("CLK ON"));
 }
+
 void command_8()
 {
+    if (!command_sent[8 - 1])
+    {
+        MIDI.sendProgramChange(113, CH_SWITCHER); // Booster
+        setOled(F("BOOST"), F("ON"));
+    }
+    else
+    {
+        MIDI.sendProgramChange(103, CH_SWITCHER); // Booster
+        setOled(F("BOOST"), F("OFF"));
+    }
+    command_sent[8 - 1] = !command_sent[8 - 1];
 }
